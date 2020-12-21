82b58e097a28cd9dcb756141db2a215e3f91a7e2
xuandien369 <xuandien369@gmail.com>
2020-12-21 13:34:09 +0700

    cmt
    cmt

--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -26,7 +26,11 @@ Route::get('newspaper/{id}/edit',"BaivietController@edit")->name('newspaper.edit
  Route::get('newspaper/account/add',"Account@addAccount")->name('newspaper.addAccount');
  Route::get('newspaper/account/follow',"Account@followAccount")->name('newspaper.followAccount');
  Route::get('newspaper/category',"Catagorys@Index")->name('newspaper.categoryIndex');
+
  Route::get('newspaper/comment',"Comment@Index")->name('newspaper.commentIndex');
+ Route::get('newspaper/comment/edit/{id}',"Comment@edit")->name('newspaper.commentedit');
+ Route::put('newspaper/comment/updateput/{id}',"Comment@update")->name('cmt.handleupdates');
+ Route::delete('newspaper/comment/delete/{id}',"Comment@delete")->name('cmt.delete');
 
  Route::get('newspaper/category/createget',"Catagorys@create")->name('hihi.create');
  Route::post('newspaper/category/createpost',"Catagorys@store")->name('hihi.store');
