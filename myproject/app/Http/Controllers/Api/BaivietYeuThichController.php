b9e2fe0e68926b8090cbc230f472d3803c5b6100
CodeChinh <khleson79929@gmail.com>
2021-01-08 20:43:57 +0700

    dd

--- myproject/app/Http/Controllers/Api/BaivietYeuThichController.php
+++ myproject/app/Http/Controllers/Api/BaivietYeuThichController.php
@@ -8,16 +8,25 @@ use App\BaiViet;
 use App\Http\Controllers\Controller;
 class BaivietYeuThichController extends Controller
 {
-
-  
-
     public function index(Request $request)
     {
+      $dem=0;
+            $list1 =YeuThichBaiViet::all();
             $baiviet=new YeuThichBaiViet();
-            $baiviet->baiviet_id=$request->id_baiviet;
-            $baiviet->user_id=$request->id_user;
-            $baiviet->save();
-          echo "successFavorite";   
+            foreach($list1 as $list){
+              if($list['baiviet_id']==$request->id_baiviet){
+                $dem++;
+              }
+            }
+            if($dem==0){
+              $baiviet->baiviet_id=$request->id_baiviet;
+              $baiviet->user_id=$request->id_user;
+              $baiviet->save();
+            echo "successFavorite";   
+            }
+            else {
+              echo "Identical";  
+            }
     }
 
     public function getAll(){
